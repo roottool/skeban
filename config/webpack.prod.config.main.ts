a45536ce198ecf776ccd7997ace22aef1628c1c1
roottool <smart.dog.lilienthal@gmail.com>
2019-07-14 00:40:01 +0900

    Created webpack config files to match dev and prod

---
+++ config/webpack.prod.config.main.ts
@@ -0,0 +1,54 @@
+// eslint-disable-next-line no-unused-vars
+import Webpack from "webpack";
+import Path from "path";
+
+import Paths from "./paths";
+
+const { appSrc, appBuild, appNodeModules } = Paths;
+
+const mainRules: Webpack.Rule[] = [
+  {
+    test: /\.ts$/,
+    include: [appSrc],
+    exclude: [appNodeModules],
+    use: [
+      {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            ["@babel/preset-env", { targets: "maintained node versions" }]
+          ]
+        }
+      },
+      {
+        loader: "eslint-loader"
+      }
+    ]
+  }
+];
+
+const mainModules: Webpack.Module = {
+  rules: mainRules
+};
+
+const mainResolve: Webpack.Resolve = {
+  extensions: [".js", ".ts"]
+};
+
+const config: Webpack.Configuration = {
+  mode: "production",
+  target: "electron-main",
+  entry: Path.join(appSrc, "main.ts"),
+  output: {
+    filename: "index.js",
+    path: appBuild
+  },
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  module: mainModules,
+  resolve: mainResolve
+};
+
+export default config;
---
+++ config/webpack.prod.config.renderer.ts
@@ -0,0 +1,56 @@
+// eslint-disable-next-line no-unused-vars
+import Webpack from "webpack";
+import Path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+import Paths from "./paths";
+
+const { appSrc, appBuild, appNodeModules, appHtml } = Paths;
+
+const rendererRules: Webpack.Rule[] = [
+  {
+    test: /\.(ts|tsx)?$/,
+    include: [appSrc, appNodeModules],
+    use: [
+      {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            ["@babel/preset-env", { targets: "maintained node versions" }]
+          ]
+        }
+      },
+      {
+        loader: "eslint-loader"
+      }
+    ]
+  }
+];
+
+const rendererModules: Webpack.Module = {
+  rules: rendererRules
+};
+
+const rendererResolve: Webpack.Resolve = {
+  extensions: [".js", ".ts", ".tsx", ".json"]
+};
+
+const config: Webpack.Configuration = {
+  mode: "production",
+  target: "electron-renderer",
+  entry: Path.join(Paths.appSrc, "index.tsx"),
+  output: {
+    filename: "index.js",
+    path: Path.resolve(appBuild, "renderer")
+  },
+  module: rendererModules,
+  resolve: rendererResolve,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: appHtml,
+      filename: Path.join(appBuild, "index.html")
+    })
+  ]
+};
+
+export default config;
