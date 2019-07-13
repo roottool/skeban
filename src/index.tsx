49026d1945fd4b27cc918001a97a2e6b7f681c7c
roottool <smart.dog.lilienthal@gmail.com>
2019-07-14 03:10:34 +0900

    Introduced typeface-roboto

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+// eslint-disable-next-line import/extensions
+import "typeface-roboto";
 
 ReactDOM.render(<App />, document.getElementById("root"));
