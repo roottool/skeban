23f447214e3f1d494a311556f146fabd8be83771
roottool <smart.dog.lilienthal@gmail.com>
2019-07-23 22:19:54 +0900

    Added the board addition button

--- src/components/LeftSideList/index.tsx
+++ src/components/LeftSideList/index.tsx
@@ -4,6 +4,9 @@ import Container from "@material-ui/core/Container";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import State from "../../State";
 
 type Props = {
   toggleLeftSideBar: (event: React.KeyboardEvent | React.MouseEvent) => void;
@@ -11,6 +14,12 @@ type Props = {
 
 const LeftSideBar: React.FC<Props> = props => {
   const { toggleLeftSideBar } = props;
+
+  const StateContainer = State.useContainer();
+
+  const handleAddButtonClicked = () => {
+    StateContainer.onBoardAdded();
+  };
 
   return (
     <div
@@ -23,6 +32,15 @@ const LeftSideBar: React.FC<Props> = props => {
           <Typography variant="h4">Skeban</Typography>
         </Toolbar>
         <Divider />
+        <StyledAddbuttonArea>
+          <Fab
+            color="primary"
+            aria-label="Add"
+            onClick={handleAddButtonClicked}
+          >
+            <AddIcon />
+          </Fab>
+        </StyledAddbuttonArea>
       </StyledLeftSideBar>
     </div>
   );
@@ -30,6 +48,11 @@ const LeftSideBar: React.FC<Props> = props => {
 
 const StyledLeftSideBar = styled(Container)`
   width: 30%;
+`;
+
+const StyledAddbuttonArea = styled.div`
+  text-align: center;
+  margin-top: 16px;
 `;
 
 export default LeftSideBar;
